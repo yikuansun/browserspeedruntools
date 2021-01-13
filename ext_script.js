0a3e3b41196d4db56db2ba5a6924fdd30740edec
Yikuan Sun <yikuansun2015@gmail.com>
2021-01-13 11:20:15 -0500

    added base files

---
+++ ext_script.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", function load(event) {
+    iframe = document.createElement("iframe");
+    
+    iframe.style.width = "250px";
+    iframe.style.backgroundImage = "bg_translucent.png";
+    iframe.style.height = "0px";
+    iframe.style.position = "fixed";
+    iframe.style.top = "0";
+    iframe.style.right = "0";
+    iframe.style.zIndex = "1000000000000000";
+    iframe.frameBorder = "none";
+    
+    iframe.src = chrome.runtime.getURL("popup.html");
+    document.body.appendChild(iframe);
+});
+
+function toggle_iframe() {
+    if (iframe.style.height == "0px"){
+        iframe.style.height = "400px";
+    } else {
+        iframe.style.height = "0px";
+    }
+}
+
+chrome.runtime.onMessage.addListener(function(msg, sender) {
+    if (msg.action == "must_toggle_iframe"){
+        toggle_iframe();
+    }
+});
